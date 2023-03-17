ff549148cb793491b4499225fe02ed1e2fe4626f
Clemen1ine <clementinemilan@gmail.com>
2023-03-17 17:10:05 +0300

    Анимация смерти слайма
    баги:
    1. во время движения персонажа по мішки, не проигрівается анимация ходьбы

--- Coin_game/Assets/Enemy.cs
+++ Coin_game/Assets/Enemy.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Animator animator;
+    
     private float health = 1;
 
     public float Health
@@ -20,7 +23,17 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void Defeated()
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Defeated()
+    {
+        animator.SetTrigger("Defeated");
+    }
+
+    private void Removwenemy()
     {
         Destroy(gameObject);
     }
