1e02f66c69b2d57661a1d3c5fb1e1a6dad553be8
Clemen1ine <clementinemilan@gmail.com>
2023-03-30 19:35:46 +0300

    save

--- Coin_game/Assets/Scripts/MouseController.cs
+++ Coin_game/Assets/Scripts/MouseController.cs
@@ -10,11 +10,15 @@ public class MouseController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
     private Vector2 targetPosition;
+    private LayerMask layerMask;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        
+        // Set up the layer mask to ignore collisions with a certain layer
+        layerMask = LayerMask.GetMask("Ignorecolliders");
     }
 
     private void Update()
@@ -27,7 +31,17 @@ public class MouseController : MonoBehaviour
         if (Vector2.Distance(transform.position, targetPosition) > 0.1f)
         {
             Vector2 direction = (targetPosition - (Vector2)transform.position).normalized;
-            rb.velocity = direction * speed;
+
+            // check for obstacles using Physics2D.Raycast and ignore collisions with a certain layer
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 0.1f, ~layerMask);
+            if (hit.collider == null)
+            {
+                rb.velocity = direction * speed;
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
 
             // set the walking animation to true
             animator.SetBool("isMoving", true);
@@ -40,4 +54,18 @@ public class MouseController : MonoBehaviour
             animator.SetBool("isMoving", false);
         }
     }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Get the normal vector of the collision
+        Vector2 normal = collision.contacts[0].normal;
+
+        // Calculate a new movement direction based on the collision normal
+        Vector2 direction = Vector2.Reflect(rb.velocity.normalized, normal);
+
+        // Apply the new direction to the character's velocity
+        rb.velocity = direction * speed * 0.2f;
+    }
 }
+
+
