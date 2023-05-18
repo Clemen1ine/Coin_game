6e9a8d8edc4387478f44ab5e28e923a2a8eb3c73
Clemen1ine <clementinemilan@gmail.com>
2023-05-18 20:30:54 +0300

    FIXED
    FIXED:
     1. Bug room triggers

--- Coin_game/Assets/Scripts/Player/MouseMovement.cs
+++ Coin_game/Assets/Scripts/Player/MouseMovement.cs
@@ -1,99 +1,97 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    public float baseMoveSpeed = 5f; // The base movement speed
-    public float shiftMultiplier = 2f; // Multiplier applied to moveSpeed when shift is held down
+    public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
 
-    private Vector2 _movement;
-    private float _attackTime = 0.25f;
-    private float _attackCounter = 0.25f;
-    private bool _isAttacking;
-    private Vector2 _startPosition;
-    private float _minimumAttackDistance = 0.1f;
+    private Vector2 destination;
+    private bool isMoving = false;
+    private float attackTime = .25f;
+    private float attackCounter = .25f;
+    private bool IsAttacking;
+    private Vector2 startPosition;
+    private float minimumAttackDistance = 0.1f;
+    private Vector2 moveDirection = Vector2.zero;
+    private Vector2 lastMoveDirection = Vector2.zero;
 
     private void Start()
     {
-        _startPosition = rb.position;
+        startPosition = rb.position;
     }
 
     private void Update()
     {
-        if (!_isAttacking)
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButton(0))
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0f;
+            destination = mousePos;
+            moveDirection = (destination - rb.position).normalized;
+
+            animator.SetFloat("Horizontal", moveDirection.x);
+            animator.SetFloat("Vertical", moveDirection.y);
+            animator.SetFloat("Speed", 1f);
+
+            if (!isMoving)
             {
-                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                _movement = (mousePosition - transform.position).normalized;
+                lastMoveDirection = moveDirection;
             }
-            else
-            {
-                _movement = Vector2.zero;
-            }
-        }
-        else
-        {
-            _movement = Vector2.zero;
+
+            isMoving = true;
         }
 
-        animator.SetFloat("Horizontal", _movement.x);
-        animator.SetFloat("Vertical", _movement.y);
-        animator.SetFloat("Speed", _movement.magnitude);
-
-        if (_movement.magnitude > 0)
+        if (isMoving && Vector2.Distance(rb.position, destination) < 0.1f)
         {
-            animator.SetBool("IsMoving", true);
-            animator.SetFloat("LastMoveX", _movement.x);
-            animator.SetFloat("LastMoveY", _movement.y);
-        }
-        else
-        {
-            animator.SetBool("IsMoving", false);
+            isMoving = false;
+            animator.SetFloat("Speed", 0f);
         }
 
-        if (_isAttacking)
+        if (IsAttacking)
         {
             rb.velocity = Vector2.zero;
-            _attackCounter -= Time.deltaTime;
-            if (_attackCounter <= 0)
+            attackCounter -= Time.deltaTime;
+            if (attackCounter <= 0)
             {
                 animator.SetBool("IsAttacking", false);
-                _isAttacking = false;
+                IsAttacking = false;
             }
         }
 
         if (Input.GetKeyDown(KeyCode.T))
         {
-            if (!_isAttacking && Vector2.Distance(rb.position, _startPosition) >= _minimumAttackDistance)
+            if (Vector2.Distance(rb.position, startPosition) >= minimumAttackDistance)
             {
-                _attackCounter = _attackTime;
+                attackCounter = attackTime;
                 animator.SetBool("IsAttacking", true);
-                _isAttacking = true;
-
-                // Reset movement input while attacking
-                _movement = Vector2.zero;
+                IsAttacking = true;
             }
         }
-    }
 
+        if (isMoving)
+        {
+            animator.SetFloat("Horizontal", moveDirection.x);
+            animator.SetFloat("Vertical", moveDirection.y);
+
+            rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.deltaTime);
+        }
+    }
+    
     private void FixedUpdate()
     {
-        if (!_isAttacking)
+        if (!isMoving)
         {
-            float currentMoveSpeed = baseMoveSpeed;
-
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            if (lastMoveDirection.x != 0f)
             {
-                currentMoveSpeed *= shiftMultiplier;
+                animator.SetFloat("Horizontal", lastMoveDirection.x);
             }
-
-            Vector2 movementVelocity = _movement * currentMoveSpeed * Time.fixedDeltaTime;
-            rb.MovePosition(rb.position + movementVelocity);
+            if (lastMoveDirection.y != 0f)
+            {
+                animator.SetFloat("Vertical", lastMoveDirection.y);
+            }
         }
     }
 }
