3927ed8dfd707057f88a6e21b50cf30e6026b522
Clemen1ine <clementinemilan@gmail.com>
2023-06-13 09:10:23 +0300

    Transform quantity with CTRL UI(Ver.)

--- Coin_game/Assets/Scripts/Inventory/InventorySlot.cs
+++ Coin_game/Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,75 +1,93 @@
 using UnityEngine;
-    using UnityEngine.EventSystems;
+using UnityEngine.EventSystems;
 
-    public class InventorySlot : MonoBehaviour, IDropHandler
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public QuantitySelectionUI quantitySelectionUI; // Reference to the QuantitySelectionUI script
+
+    public void OnDrop(PointerEventData eventData)
     {
-        public void OnDrop(PointerEventData eventData)
+        InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
+
+        // Check if the item is being dropped on the same slot
+        if (inventoryItem.parentAfterDrag == transform)
+            return;
+
+        if (transform.childCount > 0)
         {
-            InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
+            InventoryItem existingItem = transform.GetChild(0).GetComponent<InventoryItem>();
 
-            // Check if the item is being dropped on the same slot
-            if (inventoryItem.parentAfterDrag == transform)
-                return;
-
-            if (transform.childCount > 0)
+            // Check if the items can stack
+            if (existingItem.item == inventoryItem.item && existingItem.count < InventoryManager.inventoryManager.maxStackedItems)
             {
-                InventoryItem existingItem = transform.GetChild(0).GetComponent<InventoryItem>();
+                int spaceAvailable = InventoryManager.inventoryManager.maxStackedItems - existingItem.count;
+                int itemCountToTransfer = Mathf.Min(inventoryItem.count, spaceAvailable);
 
-                // Check if the items can stack
-                if (existingItem.item == inventoryItem.item && existingItem.count < InventoryManager.inventoryManager.maxStackedItems)
+                // Check if the Ctrl key is held down
+                if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
                 {
-                    int spaceAvailable = InventoryManager.inventoryManager.maxStackedItems - existingItem.count;
-                    int itemCountToTransfer = Mathf.Min(inventoryItem.count, spaceAvailable);
+                    quantitySelectionUI.OpenQuantitySelection();
 
-                    existingItem.count += itemCountToTransfer;
-                    existingItem.RefreshCount();
+                    // Wait for the player to confirm the selected quantity
+                    // You can use an event system or a button click to handle this interaction
+                    // Once the player confirms, retrieve the selected quantity from the quantitySelectionUI script
+                    // Let's assume the selected quantity is retrieved through the GetSelectedQuantity() method
 
-                    inventoryItem.count -= itemCountToTransfer;
-                    inventoryItem.RefreshCount();
+                    // Clamp the selected quantity within the valid range
+                    itemCountToTransfer = Mathf.Clamp(quantitySelectionUI.GetSelectedQuantity(), 1, itemCountToTransfer);
 
-                    if (inventoryItem.count == 0)
-                    {
-                        // The entire stack has been transferred, destroy the item object
-                        Destroy(inventoryItem.gameObject);
-                    }
+                    quantitySelectionUI.CloseQuantitySelection();
                 }
-                else
+
+                existingItem.count += itemCountToTransfer;
+                existingItem.RefreshCount();
+
+                inventoryItem.count -= itemCountToTransfer;
+                inventoryItem.RefreshCount();
+
+                if (inventoryItem.count == 0)
                 {
-                    // Slot already has an item, swap positions
-                    Transform currentItem = transform.GetChild(0);
-                    Transform originalParent = inventoryItem.parentAfterDrag;
-
-                    currentItem.SetParent(originalParent);
-                    currentItem.position = originalParent.position;
-
-                    inventoryItem.transform.SetParent(transform);
-                    inventoryItem.transform.position = transform.position;
-                    inventoryItem.parentAfterDrag = transform;
+                    // The entire stack has been transferred, destroy the item object
+                    Destroy(inventoryItem.gameObject);
                 }
             }
             else
             {
-                // Move the entire stack to an empty slot
-                InventorySlot sourceSlot = inventoryItem.parentAfterDrag.GetComponent<InventorySlot>();
+                // Slot already has an item, swap positions
+                Transform currentItem = transform.GetChild(0);
+                Transform originalParent = inventoryItem.parentAfterDrag;
+
+                currentItem.SetParent(originalParent);
+                currentItem.position = originalParent.position;
 
                 inventoryItem.transform.SetParent(transform);
                 inventoryItem.transform.position = transform.position;
                 inventoryItem.parentAfterDrag = transform;
+            }
+        }
+        else
+        {
+            // Move the entire stack to an empty slot
+            InventorySlot sourceSlot = inventoryItem.parentAfterDrag.GetComponent<InventorySlot>();
 
-                // Swap items within the source slot to fill the empty space
-                if (sourceSlot != null && sourceSlot.transform != transform)
+            inventoryItem.transform.SetParent(transform);
+            inventoryItem.transform.position = transform.position;
+            inventoryItem.parentAfterDrag = transform;
+
+            // Swap items within the source slot to fill the empty space
+            if (sourceSlot != null && sourceSlot.transform != transform)
+            {
+                InventoryItem[] itemsInSourceSlot = sourceSlot.GetComponentsInChildren<InventoryItem>();
+                foreach (InventoryItem item in itemsInSourceSlot)
                 {
-                    InventoryItem[] itemsInSourceSlot = sourceSlot.GetComponentsInChildren<InventoryItem>();
-                    foreach (InventoryItem item in itemsInSourceSlot)
+                    if (item != inventoryItem && item.parentAfterDrag == transform)
                     {
-                        if (item != inventoryItem && item.parentAfterDrag == transform)
-                        {
-                            item.parentAfterDrag = sourceSlot.transform;
-                            item.transform.SetParent(sourceSlot.transform);
-                            item.transform.position = sourceSlot.transform.position;
-                        }
+                        item.parentAfterDrag = sourceSlot.transform;
+                        item.transform.SetParent(sourceSlot.transform);
+                        item.transform.position = sourceSlot.transform.position;
                     }
                 }
             }
         }
     }
+}
